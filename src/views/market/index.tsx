1bee6ac3e46f4e9eaa1d2dbe0368a1a1e243b501
seniorblockchaindev <haoming.cui.hk@gmail.com>
2023-04-17 10:34:06 +0900

    fixed merge conflect

--- src/views/market/index.tsx
+++ src/views/market/index.tsx
@@ -371,13 +371,8 @@ const Market = ({selectedChain}: {selectedChain: any}) => {
     }
 
 		if(web3Context?.chainId !== CHAIN[1]) {
-<<<<<<< HEAD
-		  toast.error("Confirm you are on Ethereum Network!");
-		  return;
-=======
 		  toast.error("Confirm you are on Polygon Network!")
 		  return 
->>>>>>> 299e0503e8ec64a08e240183ce539ee708964e89
 		}
 
     if (selectedFile === null) { toast.error("Select File"); return; }
